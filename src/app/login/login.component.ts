23fc730c522c5790bfb5724a50a8c2fa461c247c
Me <toky.rakot@waouhstudios.com>
2020-07-03 19:03:20 +0300

    login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Page } from "tns-core-modules/ui/page";
 
 @Component({
   selector: 'ns-login',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private page: Page) { }
 
   ngOnInit(): void {
+    this.page.actionBarHidden = true;
   }
 
 }
