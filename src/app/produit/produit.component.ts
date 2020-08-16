23b2ac886c10829f4f9a32caf803d21253b005a8
katucia <mahalinafitia@gmail.com>
2020-08-16 23:42:03 +0300

    productPage

--- src/app/produit/produit.component.ts
+++ src/app/produit/produit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef  } from '@angular/core';
 import { Page} from 'tns-core-modules/ui/page/page';
 
 @Component({
@@ -6,12 +6,111 @@ import { Page} from 'tns-core-modules/ui/page/page';
   templateUrl: './produit.component.html',
   styleUrls: ['./produit.component.css']
 })
+
 export class ProduitComponent implements OnInit {
+  @ViewChild("icon", { read: ElementRef, static: false }) view: ElementRef;
+ 
+  tab = [
+    {
+      id: 1,
+      text: "MacBook air mi 2020",
+      number: 4,
+      grade: true,
+      price: "1000 ar",
+      grade2 : false,
+      favorite: false,
+      grocery: false
+    },
+    {
+      id: 2,
+      text: "MacBook air mi 2020",
+      number: 4,
+      grade: true,
+      price: "1000 ar",
+      grade2 : false,
+      favorite: false,
+      grocery: false
+    },
+    {
+      id: 3,
+      text: "MacBook air mi 2020",
+      number: 4,
+      grade: true,
+      price: "1000 ar",
+      grade2 : false,
+      favorite: false,
+      grocery: false
+    },
+    {
+      id: 4,
+      text: "MacBook air mi 2020",
+      number: 4,
+      grade: true,
+      price: "1000 ar",
+      grade2 : false,
+      favorite: false,
+      grocery: false
+    },
+    {
+      id: 5,
+      text: "MacBook air mi 2020",
+      number: 4,
+      grade: true,
+      price: "1000 ar",
+      grade2 : false,
+      favorite: false,
+      grocery: false
+    },
+    {
+      id: 6,
+      text: "MacBook air mi 2020",
+      number: 4,
+      grade: true,
+      price: "1000 ar",
+      grade2 : false,
+      favorite: false,
+      grocery: false
+    }
+  ]
 
   constructor(private page: Page) { }
 
   ngOnInit(): void {
     this.page.actionBarHidden = true;
+  }
+  
+  addOrRemovefavorite(id, i) {
+    
+    if (this.tab[i].favorite == true) {
+      this.tab[i].favorite = false;
+    } else {
+      this.tab[i].favorite= true;
+    }
+  }
+  addOrRemovegrade(id, i) {
+    
+    if (this.tab[i].grade == true) {
+      this.tab[i].grade = false;
+    } else {
+      this.tab[i].grade= true;
+    }
+  }
+  addOrRemovegrocery(id, i) {
+    
+    if (this.tab[i].grocery == true) {
+      this.tab[i].grocery = false;
+    } else {
+      this.tab[i].grocery= true;
+    }
+  }
+
+  animate() {
+    this.view.nativeElement.animate({
+      backgroundColor: "#3D5AFE",
+      duration: 500,
+      curve: "easeIn"
+    });
+
   }
 
 }
