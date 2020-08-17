5492f0dddb9ba45b336d0e67ce3c1850e911fc38
katucia <mahalinafitia@gmail.com>
2020-08-17 16:53:08 +0300

    amelioration productPage

--- src/app/produit/produit.component.ts
+++ src/app/produit/produit.component.ts
@@ -8,68 +8,44 @@ import { Page} from 'tns-core-modules/ui/page/page';
 })
 
 export class ProduitComponent implements OnInit {
-  @ViewChild("icon", { read: ElementRef, static: false }) view: ElementRef;
+  @ViewChild("icon", { read: ElementRef, static:false }) view: ElementRef;
  
   tab = [
     {
       id: 1,
       text: "MacBook air mi 2020",
       number: 4,
-      grade: true,
       price: "1000 ar",
-      grade2 : false,
-      favorite: false,
-      grocery: false
     },
     {
       id: 2,
-      text: "MacBook air mi 2020",
-      number: 4,
-      grade: true,
-      price: "1000 ar",
-      grade2 : false,
-      favorite: false,
-      grocery: false
+      text: "Telephone haute de gamme",
+      number: 5,
+      price: "1500 ar",
     },
     {
       id: 3,
-      text: "MacBook air mi 2020",
+      text: "Samsung galaxy tab 2",
       number: 4,
-      grade: true,
-      price: "1000 ar",
-      grade2 : false,
-      favorite: false,
-      grocery: false
+      price: "5400 ar",
     },
     {
       id: 4,
       text: "MacBook air mi 2020",
       number: 4,
-      grade: true,
       price: "1000 ar",
-      grade2 : false,
-      favorite: false,
-      grocery: false
     },
     {
       id: 5,
       text: "MacBook air mi 2020",
       number: 4,
-      grade: true,
       price: "1000 ar",
-      grade2 : false,
-      favorite: false,
-      grocery: false
     },
     {
       id: 6,
       text: "MacBook air mi 2020",
       number: 4,
-      grade: true,
       price: "1000 ar",
-      grade2 : false,
-      favorite: false,
-      grocery: false
     }
   ]
 
@@ -78,39 +54,4 @@ export class ProduitComponent implements OnInit {
   ngOnInit(): void {
     this.page.actionBarHidden = true;
   }
-  
-  addOrRemovefavorite(id, i) {
-    
-    if (this.tab[i].favorite == true) {
-      this.tab[i].favorite = false;
-    } else {
-      this.tab[i].favorite= true;
-    }
-  }
-  addOrRemovegrade(id, i) {
-    
-    if (this.tab[i].grade == true) {
-      this.tab[i].grade = false;
-    } else {
-      this.tab[i].grade= true;
-    }
-  }
-  addOrRemovegrocery(id, i) {
-    
-    if (this.tab[i].grocery == true) {
-      this.tab[i].grocery = false;
-    } else {
-      this.tab[i].grocery= true;
-    }
-  }
-
-  animate() {
-    this.view.nativeElement.animate({
-      backgroundColor: "#3D5AFE",
-      duration: 500,
-      curve: "easeIn"
-    });
-
-  }
-
 }
